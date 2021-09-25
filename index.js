a8168d10ade66edfb8304e82208b7f92e51c6503
Arnab Sen <arnabsen1729@gmail.com>
2021-09-25 18:50:08 +0530

    [index.js] Changed to callback

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server started on port: ${PORT}`);
-});
+const callback = () => {
+    console.log(`Server started on http://localhost:${PORT}`);
+};
+
+app.listen(PORT, callback);
