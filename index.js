2f331fca360e1dc0f4c09af6582eda649160f6ac
Arnab Sen <arnabsen1729@gmail.com>
2021-09-25 19:00:18 +0530

    GET / API added

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import express from 'express';
 const app = express();
 const PORT = 5000;
 
+app.get('/', (req, res) => {
+    res.send('Hello from GDSC-IIEST');
+});
+
 const callback = () => {
     console.log(`Server started on http://localhost:${PORT}`);
 };
